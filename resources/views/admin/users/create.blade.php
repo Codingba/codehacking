482a96b3df1981796a00b57ab9a03d95cb761898
ismet <codingba@gmail.com>
2016-05-13 18:59:38 +0200

    users whit photo create

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -26,8 +26,8 @@
                  {!! Form::password('password', ['class'=>'form-control']) !!}
              </div>
              <div class="form-group">
-                 {!! Form::label('file', 'Image:') !!}
-                 {!! Form::file('file', null, ['class'=>'form-control']) !!}
+                 {!! Form::label('photo_id', 'Image:') !!}
+                 {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
              </div>
              <div class="form-group">
                  {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
