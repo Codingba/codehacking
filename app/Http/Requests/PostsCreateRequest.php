a6eb759525729b59daae050900010f0378380503
ismet <codingba@gmail.com>
2016-05-15 10:02:16 +0200

    posts create work - except catehory

---
+++ app/Http/Requests/PostsCreateRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class PostsCreateRequest extends Request
+{
+
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'title'       => 'required',
+           // 'category_id' => 'required',
+            'photo_id'    => 'required',
+           'body'        => 'required'
+        ];
+    }
+}
